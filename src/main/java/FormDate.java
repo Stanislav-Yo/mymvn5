33c09f07e827610e959b9f9adc0751153730b75b
Stas Git <yolkhingp@gmail.com>
2025-01-13 21:36:20 +0300

    completed task

---
+++ src/main/java/FormDate.java
@@ -0,0 +1,12 @@
+public class FormDate {
+    public int day;
+    public int month;
+    public int year;
+
+    public FormDate(int day, int month, int year) {
+    Post post = new Post();
+    this.day = day;
+    this.month = month;
+    this.year = year;
+    }
+}
